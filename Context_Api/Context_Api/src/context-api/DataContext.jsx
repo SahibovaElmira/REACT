9344e124a461b3a5dd7f954294a52fa995a1ab87
SahibovaElmira <sahibovaelmira@gmail.com>
2023-10-26 03:09:39 +0300

    createContext

---
+++ Context_Api/Context_Api/src/context-api/DataContext.jsx
@@ -0,0 +1,9 @@
+import { createContext } from "react";
+const Datacontext = createContext({})
+const DatacontextProvider = () =>{
+    return(
+        <>
+        </>
+        )
+}
+export default DatacontextProvider; 
